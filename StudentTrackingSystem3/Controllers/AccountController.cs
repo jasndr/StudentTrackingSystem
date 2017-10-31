6149359806ace3574a9d4be5eea05ede1dccc8d1
jasndr <jasndr@gmail.com>
2017-10-30 15:28:45 -1000

    Fixed register account error where email was saved as username despite already specifying username in the login screen.

--- StudentTrackingSystem3/Controllers/AccountController.cs
+++ StudentTrackingSystem3/Controllers/AccountController.cs
@@ -152,7 +152,7 @@ namespace StudentTrackingSystem3.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
